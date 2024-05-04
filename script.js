8e7f66c7b9c1d6e5373a5c149477413f59c759ff
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-05-04 16:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if (year%4==0){
+		return 366;
+	}
+	else if(year%400==0){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+let year=prompt("Enter A Valid Year");
